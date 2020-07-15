edef0288680d7c7ec8c8a1e77890ca7e0629ef6b
FFlop <andrewnijmeh1@gmail.com>
2020-07-15 18:08:52 -0400

    added new file for youtube notifications using the youtube api also added  a new environnment variable in .env file for the api key

--- commands/youtube.py
+++ commands/youtube.py
@@ -2,14 +2,12 @@ import discord
 from discord.ext import commands
 import asyncio
 
-class Giveaway(commands.Cog):
+class Notifications(commands.Cog):
     
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    def start(self, ctx, days=None, title=None):
-       pass
+    
 
 def setup(bot):
     bot.add_cog(Giveaway(bot))
